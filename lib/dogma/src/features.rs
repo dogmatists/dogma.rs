a35c516722a5d9cfd833798ea18ae9023b7e6e66
Arto Bendiken <arto@bendiken.net>
2025-04-26 10:29:17 +0200

    Define feature flags for traits.

--- lib/dogma/src/features.rs
+++ lib/dogma/src/features.rs
@@ -2,6 +2,14 @@
 
 /// The set of features that are enabled in this build of the crate.
 pub static FEATURES: &[&str] = &[
+    #[cfg(feature = "collection")]
+    "collection",
+    #[cfg(feature = "countable")]
+    "countable",
+    #[cfg(feature = "labeled")]
+    "labeled",
+    #[cfg(feature = "named")]
+    "named",
     #[cfg(feature = "unstable")]
     "unstable",
 ];
--- lib/dogma/src/traits.rs
+++ lib/dogma/src/traits.rs
@@ -1,25 +1,41 @@
 // This is free and unencumbered software released into the public domain.
 
+#[cfg(feature = "collection")]
 mod collection;
+#[cfg(feature = "collection")]
 pub use collection::*;
 
+#[cfg(feature = "collection")]
 mod collection_mut;
+#[cfg(feature = "collection")]
 pub use collection_mut::*;
 
+#[cfg(feature = "countable")]
 mod countable;
+#[cfg(feature = "countable")]
 pub use countable::*;
 
+#[cfg(feature = "labeled")]
 mod labeled;
+#[cfg(feature = "labeled")]
 pub use labeled::*;
 
+#[cfg(feature = "countable")]
 mod maybe_countable;
+#[cfg(feature = "countable")]
 pub use maybe_countable::*;
 
+#[cfg(feature = "labeled")]
 mod maybe_labeled;
+#[cfg(feature = "labeled")]
 pub use maybe_labeled::*;
 
+#[cfg(feature = "named")]
 mod maybe_named;
+#[cfg(feature = "named")]
 pub use maybe_named::*;
 
+#[cfg(feature = "named")]
 mod named;
+#[cfg(feature = "named")]
 pub use named::*;
